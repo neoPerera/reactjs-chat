fc6aa1ab292b4abc3d32b4e297634512f32bd60e
neoPerera <chanuthnilushan@gmail.com>
2020-05-02 22:30:05 +0530

    changed name

--- src/endpoint/endpoint.js
+++ src/endpoint/endpoint.js
@@ -1,16 +1,16 @@
 //heroku server
 //https://neo-chatv1.herokuapp.com/
 
-// const ENDPOINT = "https://neo-chatv1.herokuapp.com/";
-// const LOGINENDPOINT = "https://neo-chatv1.herokuapp.com/login";
-// const GETKEY = "https://neo-chatv1.herokuapp.com/getkey";
-// const AUTHENDPOINT = "https://neo-chatv1.herokuapp.com/auth";
+const ENDPOINT = "https://neo-chatv1.herokuapp.com/";
+const LOGINENDPOINT = "https://neo-chatv1.herokuapp.com/login";
+const GETKEY = "https://neo-chatv1.herokuapp.com/getkey";
+const AUTHENDPOINT = "https://neo-chatv1.herokuapp.com/auth";
 
 //localhost
 
-const ENDPOINT = "http://localhost:5000/";
-const LOGINENDPOINT = "http://localhost:5000/login";
-const GETKEY = "http://localhost:5000/getkey";
-const AUTHENDPOINT = "http://localhost:5000/auth";
+// const ENDPOINT = "http://localhost:5000/";
+// const LOGINENDPOINT = "http://localhost:5000/login";
+// const GETKEY = "http://localhost:5000/getkey";
+// const AUTHENDPOINT = "http://localhost:5000/auth";
 
 export { ENDPOINT, LOGINENDPOINT, GETKEY,AUTHENDPOINT };
